8846bf021564aca0f97ccf3359615cd051ce5185
liqiaona <lqnstar@163.com>
2018-11-21 14:34:06 +0800

    删除：实验动画相关

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,12 @@
 import { OverlayContainer } from '@angular/material';
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations:[
-    trigger('square', 
-      [
-        state('green', style({'background-color':'green', 'height':'100px', 'transform':'translateY(-100%)'})),
-        state('red', style({'background-color': 'red', 'height':'100px', 'transform':'translateY(100%)'})),
-        transition('green => red', animate('5s ease-in')),
-        transition('red => green', animate('8s ease-out'))
-      ]
-    )
-  ]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  suqareState: string;
   darkTheme = false;
   constructor(private oc: OverlayContainer){
 
@@ -26,8 +14,5 @@ export class AppComponent {
   switchTheme(dark) {
     this.darkTheme = dark;
     this.oc.themeClass = dark ? 'myapp-dark-theme' : null;
-  }
-  onClick() {
-    this.suqareState = this.suqareState === 'red' ? 'green' : 'red';
   }
 }
