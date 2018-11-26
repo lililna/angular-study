d0bf51d53e75b48a5c2a37013dad06a2098b4299
liqiaona <lqnstar@163.com>
2018-11-26 08:50:35 +0800

    添加：添加与删除动态（query&&stagger）

--- src/app/project/project-list/project-list.component.ts
+++ src/app/project/project-list/project-list.component.ts
@@ -4,13 +4,15 @@ import { Component, OnInit, HostBinding } from '@angular/core';
 import { MdDialog } from '@angular/material';
 import { NewProjectComponent } from '../new-project/new-project.component';
 import { InviteComponent } from '../invite/invite.component';
+import { listAnimation } from '../../anims/list.anim';
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.scss'],
   animations: [
-    slideToRight
+    slideToRight,
+    listAnimation
   ]
 })
 export class ProjectListComponent implements OnInit {
@@ -19,11 +21,13 @@ export class ProjectListComponent implements OnInit {
 
   projects = [
     {
+      id: 1,
       name: "企业协作平台1",
       desc: "这是一个企业内部项目1",
       coverImg: "assets/images/covers/1.jpg"
     },
     {
+      id: 2,
       name: "企业协作平台2",
       desc: "这是一个企业内部项目2",
       coverImg: "assets/images/covers/2.jpg"
@@ -45,6 +49,20 @@ export class ProjectListComponent implements OnInit {
     const dialogRef = this.dialog.open(NewProjectComponent,{data:{title: '新增项目'}});
     dialogRef.afterClosed().subscribe(result => {
       console.log(result);
+      this.projects = [...this.projects, 
+        {
+          id: 3, 
+          name: '一个新项目',
+          desc: '这是一个新项目',
+          coverImg: "assets/images/covers/3.jpg"
+        },
+        {
+          id: 4, 
+          name: '又一个新项目',
+          desc: '这是又一个新项目',
+          coverImg: "assets/images/covers/4.jpg"
+        }
+      ]
     })
   }
 
@@ -56,10 +74,11 @@ export class ProjectListComponent implements OnInit {
     const dialogRef = this.dialog.open(NewProjectComponent, {data: {title: '编辑项目'}});
   }
 
-  launchDelDialog() {
+  launchDelDialog(project) {
     const dialogRef = this.dialog.open(ConfirmDialogComponent,{data:{title: '删除项目', content: '您确定删除该项目吗？'}});
     dialogRef.afterClosed().subscribe(result => {
       console.log(result);
+      this.projects = this.projects.filter(p => p.id !== project.id)
     })
   }
 
