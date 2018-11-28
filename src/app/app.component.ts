41b0015485ad01636565569fefa2b86050d7a6c3
liqiaona <lqnstar@163.com>
2018-11-28 09:20:31 +0800

    添加：依赖性注入

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
+import { environment } from './../../taskmgr/src/environments/environment';
 import { OverlayContainer } from '@angular/material';
-import { Component } from '@angular/core';
+import { Component, ReflectiveInjector, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,54 @@ import { Component } from '@angular/core';
 export class AppComponent {
   darkTheme = false;
   constructor(private oc: OverlayContainer){
-
+    const injector = ReflectiveInjector.resolveAndCreate([
+      { provide: Person, useClass: Person },
+      { provide: Address, useFactory: () => {
+          if(environment.production) {
+            return new Address('北京','北京','朝阳区','xx 街道 xx 号');
+          } else {
+            return new Address('西藏','拉萨','xx 区','xx 街道 xx 号');
+          }
+        } 
+      },
+      { provide: Id, useFactory: () => {
+          return Id.getInstance('idcard');
+        } 
+      },
+    ]);
+    const person = injector.get(Person);
+    console.log(99999999);
+    console.log(JSON.stringify(person));
   }
   switchTheme(dark) {
     this.darkTheme = dark;
     this.oc.themeClass = dark ? 'myapp-dark-theme' : null;
+  }
+}
+class Id {
+  static getInstance(type: string): Id {
+    // 设置
+    return new Id();
+  }
+}
+class Address {
+  province: string;
+  city: string;
+  district: string;
+  street: string;
+  constructor(province, city, district, street) {
+    this.province = province;
+    this.city = city;
+    this.district = district;
+    this.street = street;
+  }
+}
+
+class Person {
+  id: Id;
+  address: Address;
+  constructor(@Inject(Id) id, @Inject(Address) address) {
+    this.id = id;
+    this.address = address;
   }
 }
