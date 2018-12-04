8668a45df53d444435e9c6f8f5f2c6bafbb1fc18
liqiaona <lqnstar@163.com>
2018-12-04 10:00:23 +0800

    依赖性注入

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -29,6 +29,9 @@ import 'hammerjs';
     SidebarComponent,
     AppRoutingModule,
     BrowserAnimationsModule
+  ],
+  providers: [
+    {provide:'BASE_CONFIG', useValue: 'http://localhost:3000'}
   ]
 })
 export class CoreModule {
