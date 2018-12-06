988133ea5312b97e9e2d7f86db577a6ab892506f
liqiaona <lqnstar@163.com>
2018-12-06 16:48:54 +0800

    添加：登录的表单验证

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import{ FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  form: FormGroup;
+  constructor(private fb: FormBuilder) { }
   ngOnInit() {
+    // this.form = new FormGroup ({
+    //   email: new FormControl('li@163.com', Validators.compose([Validators.required, Validators.email])),
+    //   password: new FormControl('', Validators.required)
+    // })
+    this.form = this.fb.group({
+      email: ['li@163.com',Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit({value, valid}, ev: Event) {
+    ev.preventDefault();
+    console.log(JSON.stringify(value));
+    console.log(JSON.stringify(valid));
   }
 
 }
